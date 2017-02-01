60bd878d95e600390c3109c48143776e5808d162
Sophie <sophie.k.mclean@btinternet.com>
2017-02-01 23:51:08 +0000

    exercise37 final version

--- Chapter19/ex37.rb
+++ Chapter19/ex37.rb
@@ -3,3 +3,18 @@ def happy
   puts "yay"
 end
 happy
+
+
+
+
+
+puts x = " happy\\dog"
+puts b = "sdddc\'dsfsd"
+puts f= "sdfsdf\"dfsd"
+puts s= "dfdfdf\adsfds"
+puts b = "cv\bsdfdsf"
+puts h= "dfddfs\fwerr"
+puts r= "vcxxcv\ndsf"
+puts t= "sdfsd\tdgdf"
+puts k = "sdfsfd\afdf"
+puts u= "dfdfgdf\vdfgdg"
