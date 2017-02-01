29686151f8bd96120d456d22d8e93d5a021d1410
Sophie <sophie.k.mclean@btinternet.com>
2017-02-01 09:42:38 +0000

    Exercise 26 amended

--- Chapter14/ex26.rb
+++ Chapter14/ex26.rb
@@ -69,7 +69,7 @@ puts "This should be five: #{five}"
 
 
 
-def secret_formula(started)
+def Ex25.secret_formula(started)
   jelly_beans = started * 500
    jars = jelly_beans / 1000
   crates = jars / 100
@@ -78,15 +78,14 @@ end
 
 
 start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+beans, jars, crates = Ex25.secret_formula(start_point)
 
 puts "With a starting point of: #{start_point}"
 puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point/ 10
 puts "We can also do that this way:"
-puts   secret_formula(start_point)
-puts "We'd have #{beans beans}, #{jars} jars, and #{crates} crates."
+puts "We'd have %s beans, %d jars, and %d crates." % Ex25.secret_formula(start_point)
 
 
 sentence = "All good things come to those who wait."
